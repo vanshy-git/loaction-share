26a6accd4919cfd0c29b825f9fc6f22a223d1e1b
unknown <vansh.y.v.y@gmail.com>
2025-12-06 18:10:39 +0530

    changed server

--- server.js
+++ server.js
@@ -216,7 +216,8 @@ app.get("/location/:id", (req, res) => {
   res.json(session);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running at http://localhost:" + PORT);
 });
+
